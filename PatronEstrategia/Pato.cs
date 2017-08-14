9e316105737785a7392f1624893b0df6d3b9a0b3
Albert <albert.capdevila@buclenet.com>
2017-08-14 18:29:34 +0200

    Esquema inicial con una clase abstracta Pato y dos subtipos

---
+++ PatronEstrategia/Pato.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PatronEstrategia
+{
+    public abstract class Pato
+    {
+        public void Graznar()
+        {
+            Console.WriteLine("¡Cuac, cuac!");
+        }
+
+        public void Nadar()
+        {
+            Console.WriteLine("Nadando");
+        }
+
+
+        public abstract void Mostrar();
+    }
+
+    public class PatoDomestico : Pato
+    {
+        public override void Mostrar()
+        {
+            Console.WriteLine("Soy un pato doméstico");
+        }
+    }
+
+    public class PatoPelirrojo : Pato
+    {
+        public override void Mostrar()
+        {
+            Console.WriteLine("Soy un pato doméstico");
+        }
+    }
+}
---
+++ PatronEstrategia/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PatronEstrategia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var patoDomestico  = new PatoDomestico();
+
+            patoDomestico.Mostrar();
+            patoDomestico.Graznar();
+            patoDomestico.Nadar();
+          
+
+            var patoPelirrojo = new PatoPelirrojo();
+
+            patoPelirrojo.Mostrar();
+            patoPelirrojo.Graznar();
+            patoPelirrojo.Nadar();
+            
+            Console.ReadLine();
+        }
+    }
+}
