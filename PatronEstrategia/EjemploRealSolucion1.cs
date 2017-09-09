aa75833789f2eae5ccc9438655b239e8e879329e
Albert <albert.capdevila@buclenet.com>
2017-09-09 17:07:24 +0200

    EjemploRealSolucionConPatron

---
+++ PatronEstrategia/EjemploRealSolucion1.cs
@@ -0,0 +1,48 @@
+﻿namespace PatronEstrategia
+{
+    public class ValidadorPeticiones
+    {
+        public bool Validar(Peticion peticion)
+        {
+            switch (peticion.TipoPeticion)
+            {
+                case TipoPeticion.FormaDePago:
+                    return ValidarFormaDePago(peticion);
+
+                case TipoPeticion.OrdenDeCobro:
+                    return ValidarOrdenDeCobro(peticion);
+
+                default:
+                    return false;
+            }
+        }
+
+
+        private bool ValidarOrdenDeCobro(Peticion peticion)
+        {
+            /**/
+
+            return true;
+        }
+
+        private bool ValidarFormaDePago(Peticion peticion)
+        {
+            /**/
+
+            return true;
+        }
+
+        /* Un método de validación por cada tipo de petición */
+    }
+
+    public class Peticion
+    {
+        public TipoPeticion TipoPeticion { get; set; }
+    }
+
+    public enum TipoPeticion
+    {
+        FormaDePago,
+        OrdenDeCobro
+    }
+}
---
+++ PatronEstrategia/EjemploRealSolucionConPatron.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronEstrategia
+{
+    public class ValidadorPeticionesConPatronEstrategia
+    {
+        private readonly IEstrategiaValidacion _estrategiValidacion;
+
+        public ValidadorPeticionesConPatronEstrategia(IEstrategiaValidacion estrategiValidacion)
+        {
+            _estrategiValidacion = estrategiValidacion;
+        }
+
+        public bool Validar(Peticion peticion)
+        {
+            return _estrategiValidacion.Validar(peticion);
+        }
+    }
+
+    public interface IEstrategiaValidacion
+    {
+        bool Validar(Peticion peticion);
+    }
+
+}
